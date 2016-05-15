4f93156154013abc67dc7e1959df408f0ec53f87
Quentin Monmert <qmonmert@macbook-pro-de-cedric.home>
2016-05-15 13:31:56 +0200

    Add CRUD operations on items

--- src/app/angularfire2-ippon.component.ts
+++ src/app/angularfire2-ippon.component.ts
@@ -10,9 +10,27 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class Angularfire2IpponAppComponent {
 
   items: FirebaseListObservable<any[]>;
+  nbItems: number = 0;
   
   constructor(af: AngularFire) {
     this.items = af.database.list('/items');
+  }
+  
+  addItem(textNewItem: string) {
+    this.items.push( { text: textNewItem } );
+    textNewItem = '';
+  }
+  
+  updateItem(keyItem: string, textUpdateItem: string) {
+    this.items.update(keyItem, {text: textUpdateItem});
+  }
+  
+  deleteItem(keyItem: string) {
+    this.items.remove(keyItem);
+  }
+  
+  deleteAllItems() {
+    this.items.remove();
   }
   
 }
